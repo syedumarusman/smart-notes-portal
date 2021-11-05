01f547a597f45eac543feb742ebeb8dc7a630adb
Stephen Forte <stephenforte@mail.adelphi.edu>
2021-11-05 01:11:41 -0400

    Initial upload file implementation.

--- frontend-ui/src/views/Manuscript.vue
+++ frontend-ui/src/views/Manuscript.vue
@@ -2,6 +2,14 @@
   <div class="row">
     <div class="col">
       <h2>Manuscript</h2>
+      <input type="file" id="file" ref="file" v-on:change="handleFileUpload()"/>
+      <button
+        class="btn btn-primary"
+        id="generateManuscript"
+        @click.prevent="generateManuscript"
+      >
+        Generate
+      </button>
     </div>
   </div>
 </template>
@@ -9,6 +17,33 @@
 <script>
 export default {
   name: "Manuscript",
+
+  methods: {
+    handleFileUpload(){
+      this.file = this.$refs.file.files[0];
+    },
+    async generateManuscript() {
+      let formData = new FormData();
+
+      formData.append('file', this.file);
+
+      // axios.post( '/upload',
+      //     formData,
+      //     {
+      //     headers: {
+      //         'Accept': 'application/json',
+      //         'Content-Type': 'multipart/form-data'
+      //     }
+      //   }
+      // )
+      // .then(function(){
+      //     console.log('success');
+      // })
+      // .catch(function(){
+      //     console.log('failed');
+      // });
+    },
+  },
 };
 </script>
 
